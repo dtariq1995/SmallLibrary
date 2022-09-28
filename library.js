26570f054b5ae25d72b768cbedb82247fc5c9b0a
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2022-09-28 02:46:11 -0500

    Finished basic CSS styling for Library.

--- library.js
+++ library.js
@@ -6,6 +6,7 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    myLibrary.push(this);
 }
 
 Book.prototype.info = function() {
@@ -18,35 +19,42 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-function displayBook() {
+function displayBook(myLibrary) {
 
+    let libarySize = myLibrary.length;
     const displayArea = document.getElementById("grid-area");
-    const card = document.createElement("div");
-    displayArea.appendChild(card);
-    card.classList.add("card");
-    const bookTitle = document.createElement("div");
-    const authorName = document.createElement("div");
-    const numPages = document.createElement("div");
-    const readOrNot = document.createElement("div");
-    card.append
+
+    for (let book = 0; book < libarySize; book++) {
+
+
+        const card = document.createElement("div");
+        displayArea.appendChild(card);
+        card.classList.add("card");
+    
+        const bookTitle = document.createElement("div");
+        bookTitle.classList.add("title");
+        bookTitle.textContent = myLibrary[book].title;
+    
+        const authorName = document.createElement("div");
+        authorName.classList.add("author");
+        authorName.textContent = "Author: " + myLibrary[book].author;
+    
+        const numPages = document.createElement("div");
+        numPages.classList.add("num-pages");
+        numPages.textContent = "Number of pages: " + myLibrary[book].pages;
+    
+        const readOrNot = document.createElement("div");
+        readOrNot.classList.add("read");
+        readOrNot.textContent = myLibrary[book].read;
+    
+        card.append(bookTitle, authorName, numPages, readOrNot);
+    }
 
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet.");
-addBookToLibrary(theHobbit);
-console.log(theHobbit.info());
-console.log(myLibrary);
-displayBook();
-displayBook();
-displayBook();
-displayBook();
-displayBook();
-displayBook();
-displayBook();
-displayBook();
-displayBook();
-displayBook();
-displayBook();
-displayBook();
-displayBook();
-displayBook();
+const theHobbit = new Book("THE HOBBIT", "J.R.R. Tolkien", 295, "Not read yet");
+const fakeBook = new Book("FAKE BOOK", "Me", 100, "Read");
+
+
+
+displayBook(myLibrary);
